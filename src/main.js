b258c176940f93fe31dd0074838bb8efb05796c1
YogiSly <84290221+YogiSly@users.noreply.github.com>
2022-12-21 02:54:08 +0300

    add:sass,delete:chartjs

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index.js'
 import store from './store/index.js'
-
+import './assets/scss/styles.scss'
 
 
 
--- src/views/ViewMain.vue
+++ src/views/ViewMain.vue
@@ -1,69 +1,35 @@
 <template>
   <div>
     <router-link to="/"><img src="../assets/images/logo.svg" alt=""></router-link>
-    <GChart style="width: 500px" type="PieChart" :data="chartDаta" :options="chartOptions" />
-    <div>
-      <ul>
-        <li v-for="goal in getGoals" :key="goal.id">{{ goal.name }}</li>
-      </ul>
-    </div>
+    <ul class="goals" style="width: 300px;">
+      <li v-for="goal in getGoals" :key="goal.id">{{ goal.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script >
-import { GChart } from "vue-google-charts";
-// import { mapActions } from 'vuex'
 import { mapGetters } from 'vuex';
-
 
 export default {
   name: "App",
   components: {
-    GChart
+
   },
   data() {
     return {
-      // chartDаta: [
-      //   ["name", "value"],
-      // ],
-      chartOptions: {
-        title: "Company Performance",
-        subtitle: "blog in total: languages",
-        is3D: true,
-        // legend: { position: 'top', textStyle: { color: 'blue', fontSize: 16 } }
-        tooltip: { ignoreBounds: true }
-      }
+
     };
   },
   computed: {
     ...mapGetters([
       'getGoals',
     ]),
-    chartDаta(){
-      const arrData = []
-      arrData.push(["name", "value"])
-      // console.log('sdsd',this.getGoals);
-      let newArrData = []
-      if (this.getGoals) {
-        const arrMini = this.getGoals.map(arr => [arr.name, '10'])
-        newArrData = [...arrData, ...arrMini]
-      }
-      console.log('newArrData', newArrData);
-      console.log('this',this);
-      this['get chartDаta']
-      return newArrData
-    }
   },
   methods: {
 
-    test () {
-      
-    }
   },
   mounted(){
-    this.$store.dispatch('setGoals')
-  
-     
+    this.$store.dispatch('setGoals') 
   }
 
 }
