34432f7fe3ab2c0094b0d1bbb1f16c37341edd11
YogiSly <84290221+YogiSly@users.noreply.github.com>
2023-02-20 23:17:35 +0300

    add:firebase

--- src/views/ViewAuth.vue
+++ src/views/ViewAuth.vue
@@ -3,3 +3,32 @@
     auth
   </div>
 </template>
+<script>
+import { collection, addDoc } from "firebase/firestore"
+// the firestore instance
+import db from '../firebase/init.js'
+export default {
+  created() {
+    this.createUser()
+  },
+  methods: {
+    async createUser() {
+      // 'users' collection reference
+      const colRef = collection(db, 'users')
+      // data to send
+      const dataObj = {
+        firstName: 'John',
+        lastName: 'Doe',
+        dob: '1990'
+      }
+
+      // create document and return reference to it
+      const docRef = await addDoc(colRef, dataObj)
+
+      // access auto-generated ID with '.id'
+      console.log('Document was created with ID:', docRef.id)
+    }
+  }
+}
+
+</script>
